5f9798866b13a184230ba358f7b1b6b63b2b4d36
Kye Buffery <kyebuffery14@gmail.com>
2018-11-05 20:20:19 +0000

    added json to the api action

--- actions/api.js
+++ actions/api.js
@@ -1,9 +1,14 @@
 import axios from '../axios/axios';
 import { setMedia } from './state';
+import media from '../static/films.json';
 
 export const getMedia = () => dispatch => {
-	axios.get('media').then(({ data }) => {
-		const media = data.data;
-		dispatch(setMedia(media))
-	});
+	axios.get('media')
+		.then(({ data }) => {
+			const media = data.data;
+			dispatch(setMedia(media))
+		})
+		.catch(()=>{
+			dispatch(setMedia(media))
+		}) 
 };
